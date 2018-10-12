bdf0714791ba4a571e6803e83af23c3d6ad74149
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-10-11 22:32:26 -0400

    Building and Using a Custom Observable from Scratch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,10 +11,46 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = interval(1000);
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log('Number - ' + number);
+    // const myNumbers = interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log('Number - ' + number);
+    //   }
+    // );
+
+    const myObservable = Observable.create(
+      (observer: Observer<string>) => {
+        setTimeout(
+          () => {
+            observer.next('first package');
+          }
+        , 2000);
+        setTimeout(
+          () => {
+            observer.next('second package');
+          }
+        , 4000);
+        setTimeout(() => {
+          // observer.error('this does not work');
+          observer.complete();
+        }, 5000);
+        setTimeout(
+          () => {
+            observer.next('third package');
+          }
+        , 6000);
+      }
+    );
+
+    myObservable.subscribe(
+      (data: string) => {
+        console.log(data);
+      },
+      (error: string) => {
+        console.log(error);
+      },
+      () => {
+        console.log('Completed');
       }
     );
   }
